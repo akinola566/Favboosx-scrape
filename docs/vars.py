ba808c230be1085a20ab9a1b2598488e1de49138
akinola566 <akinolaa692@gmail.com>
2025-04-26 12:34:13 +0100

    Create vars.py

---
+++ docs/vars.py
@@ -0,0 +1,6 @@
+# vars.py
+
+# Aviator last results area (adjust later if needed)
+last_game_result_xpath = "/html/body/app-root/app-game/div/div[1]/div[2]/div/div[2]/div[1]/app-stats-widget/div/div[1]/div/app-bubble-multiplier[1]/div"
+
+aviator_url = "https://22bet-b.com/slots"
